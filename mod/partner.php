1a2e233e9b543247b8940713b097c9028a523e09
ViniTrabalho <vinicius.turu@gmai.com>
2022-11-03 16:08:40 -0300

    Inserção de apoios

--- mod/partner.php
+++ mod/partner.php
@@ -1,6 +1,6 @@
 <?php
 
-$domain = 'http://localhost/spee';
+$domain = 'http://spee.abimaq.org.br';
 
 ?>
     <h2>Patrocínio master</h2>
@@ -39,12 +39,39 @@ $domain = 'http://localhost/spee';
     <h2>Apoio</h2>
     <div class="partners">
         <div class="partner apoio">
+            <img src="<?php echo $domain; ?>/images/apoio/abag.png" alt="">
+        </div>
+        <div class="partner apoio">
+            <img src="<?php echo $domain; ?>/images/apoio/abdib.png" alt="">
+        </div>
+        <div class="partner apoio">
             <img src="<?php echo $domain; ?>/images/apoio/abnt.png" alt="">
+        </div>
+        <div class="partner apoio">
+            <img src="<?php echo $domain; ?>/images/apoio/abramilho.png" alt="">
+        </div>
+        <div class="partner apoio">
+            <img src="<?php echo $domain; ?>/images/apoio/acirp.png" alt="">
+        </div>
+        <div class="partner apoio">
+            <img src="<?php echo $domain; ?>/images/apoio/anfavea.png" alt="">
+        </div>
+        <div class="partner apoio">
+            <img src="<?php echo $domain; ?>/images/apoio/bioenergia.png" alt="">
         </div>
         <div class="partner apoio">
             <img src="<?php echo $domain; ?>/images/apoio/cseag.png" alt="">
         </div>
         <div class="partner apoio">
             <img src="<?php echo $domain; ?>/images/apoio/csei.png" alt="">
+        </div>
+        <div class="partner apoio">
+            <img src="<?php echo $domain; ?>/images/apoio/faesp.png" alt="">
+        </div>
+        <div class="partner apoio">
+            <img src="<?php echo $domain; ?>/images/apoio/fiepe.png" alt="">
+        </div>
+        <div class="partner apoio">
+            <img src="<?php echo $domain; ?>/images/apoio/udop.png" alt="">
         </div>
     </div>
